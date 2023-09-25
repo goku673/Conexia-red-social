c4a906b43c24db5d570d6c8951f76ed97c9a99bb
goku673 <zapatamorato@gmail.com>
2023-09-25 12:50:08 -0400

    conection  a MySQL

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,41 @@
 import express from 'express';
-const app = express()
-app.use(express.json())
+import { Connection, createConnection } from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const PORT = 3000 
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-})
+
+
+const app = express();
+app.use(express.json());
+const port: number = 3006;
+
+
+
+
+const connection: Connection = createConnection({
+  host: String(process.env.HOST),
+  port: Number(process.env.PORT), // Convierte process.env.PORT a número
+  user: String(process.env.USER),
+  password: String(process.env.PASSWORD),
+  database: String(process.env.DATABASE),
+});
+
+
+connection.connect((error: Error | null) => {
+  if (error) {
+    console.error('Error al conectar a la base de datos: ', error);
+  } else {
+    console.log('Conexión exitosa a MySQL :)');
+
+
+
+
+    //---------------------Servidor iniando.
+    app.listen(port, () => {
+      console.log('Servidor iniciado en el puerto '+port);
+    });
+  }
+});
+
+
