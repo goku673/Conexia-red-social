eb3e197ec45b4151146b14be10e25c9fae808d84
goku673 <zapatamorato@gmail.com>
2023-09-28 20:23:41 -0400

    empezando  a hacer rutas

--- api/servidor.js
+++ api/servidor.js
@@ -1,7 +1,6 @@
 const rutaPrincipal = require('./Routes/index.js');
 const express = require('express');
 const cookieParser = require('cookie-parser');
-//const bodyParser = require('body-parser'); ya obsolteto
 const morgan = require('morgan');
 const cors = require('cors');
 
@@ -11,18 +10,25 @@ servidor.use(express.urlencoded({ extended: true, limit: '200mb' }));//recibir d
 servidor.use(express.json({ limit: '200mb' }));//permite recibir datos json 
 servidor.use(cookieParser());//permite mandar info por cookies;
 servidor.use(morgan('dev'));// registro de solicitudes alas rutas;
-servidor.use(cors()); // permite todas las solicitudes de cualquier origen ; 
 // cuando instalesmo passport  hacemos servidor.use(passport.initialize());
 
-servidor.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  });
-
-
+const dominioPermitido = '*';
+// forma antigua;
+// servidor.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+//   });
+//forma mas moderna
+const opcionesCors = {
+  origin: dominioPermitido,
+  credentials: true,   // solicitudes con credenciales por emjemplo cookies desde el dominio permitido 
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+servidor.use(cors(opcionesCors));
 servidor.use('/', rutaPrincipal);
 
 
