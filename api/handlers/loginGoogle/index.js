c9e22340b08eb6b5234564b86c5cd1ece54c07f3
goku673 <zapatamorato@gmail.com>
2023-10-05 20:21:13 -0400

    notificaciones sendGrid funcionando correctamente

--- api/handlers/loginGoogle/index.js
+++ api/handlers/loginGoogle/index.js
@@ -1,15 +1,39 @@
-
+const sgMail  = require ('@sendgrid/mail');
+const dontenv = require('dotenv');
+dontenv.config();
+sgMail.setApiKey(process.env.CONEXIA_API_KEY);
 
 const logGoogle = async(req,res) => {
     try {
         const user = req.user;
         if(user){
-          return  res.status(299).json(
-            {
-                message : "usuario creado con exito ",
-                user : user,
-            }
-          )
+         //configuramos el objeto para enviar el correo 
+         const msg = {
+            to : user.email, // sacamos el email de usuario, 
+            from : process.env.MY_EMAIL,
+            subject : `BIENBENIDO ${user.nombre} A NUESTRA APLICACION`,
+            text: 'Estamos contentos de que te hayas unido a nuestra aplicación. ¡Disfrutala!',
+            html : `
+            <div>
+                 <h1>BIENBENIDO ${user.nombre} a nuestra red social </h1>
+                 <br>
+                 <p>Estamos  contentos de que te hayas unido a nuestra plataforma, Disfrutala </p>
+                 <p>Espero que  te diviertas! </p>
+            </div>
+         `
+         }; // con esto ya configuramos nuestro objeto de correo,
+
+         //enviamos el email
+         sgMail.send(msg).then(() => {
+            console.log("Correo de bienbenida enviada con exito");
+         }).catch((error) => {
+             console.log('Error al enviar el correo', error.response.body);
+         });
+
+         return res.status(299).json({
+             message : 'usuario registrado con exito',
+             user :user,
+         })
         }
         return res.status(401).json({message: 'autenticacion con google fallida' });
     } catch (error) {
